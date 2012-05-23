3eb62663b4754177b79ffc803d3bfb359e2ccb4a
Andre Schweighofer <nopunktfjutscha@gmail.com>
2012-05-23 23:21:05 +0200

    all buttons (fb, twitter, +1), all attributes

--- lib/so_social/view_helpers.rb
+++ lib/so_social/view_helpers.rb
@@ -4,15 +4,20 @@ module SoSocial
       raw(
         <<-eos
           <script src='http://connect.facebook.net/en_US/all.js#xfbml=1'></script>
-          <fb:like href='#{options[:href]}' layout='#{options[:layout]}' show_faces='#{options[:show_faces]}' width='#{options[:width]}'></fb:like>
+          <fb:like href='#{options[:href]}' send='#{options[:send]}' layout='#{options[:layout]}'
+            show_faces='#{options[:show_faces]}'  width='#{options[:width]}'  action='#{options[:action]}'
+            font='#{options[:font]}'  colorscheme='#{options[:colorscheme]}'  ref='#{options[:ref]}'></fb:like>
         eos
-      )
+      ) 
     end
   
     def twitter_button(options = {})
       raw(
         <<-eos
-          <a class='twitter-share-button left' data-count='horizontal' data-hashtags='' data-text='' data-via='' href='http://twitter.com/share'>
+          <a class='twitter-share-button left' data-url='#{options[:url]}' data-via='#{options[:via]}' data-text='#{options[:text]}' 
+            data-related='#{options[:related]}' data-count='#{options[:count]}' data-lang='#{options[:lang]}' 
+            data-counturl='#{options[:counturl]}' data-hashtags='#{options[:hashtags]}' data-size='#{options[:size]}' data-dnt='#{options[:dnt]}'
+            href='http://twitter.com/share'>
           <script src='http://platform.twitter.com/widgets.js' type='text/javascript'></script>
           </a>
         eos
@@ -23,7 +28,10 @@ module SoSocial
       raw(
         <<-eos
           <script src='https://apis.google.com/js/plusone.js' type='text/javascript'></script>
-          <div class='g-plusone gplusshare'></div>
+          <div class='g-plusone gplusshare' data-href='http://fidrelity.at' data-size='#{options[:size]}' 
+            data-annotation='#{options[:annotation]}' data-width='#{options[:width]}' data-align='#{options[:align]}'
+            data-expandTo='#{options[:expand_to]}' data-callback='#{options[:callback]}' 
+            data-onstartinteraction='#{options[:on_start_interaction]}' data-onendinteraction='#{options[:on_end_interaction]}'></div>
         eos
       )
     end 
